0cc4fa1a27c06d783c4810b242210089ff38ed0e
0xInle <xsk8er26x@gmail.com>
2025-02-08 16:27:11 +0300

    Добавлен код змененения иконки и функционал копирования в буфер

--- app/js/main.js
+++ app/js/main.js
@@ -27,6 +27,23 @@ const mainSearch = document.querySelector('.main__search');
 mainBtn.addEventListener('click', function () {
   mainContent.style.display = 'flex';
 });
+const inpBtn = document.querySelectorAll('.main__inp-btn');
+inpBtn.forEach(button => {
+  button.addEventListener('click', function () {
+    const li = this.closest('li');
+    const input = li.querySelector('.main__inp-content');
+    const copyIcon = this.querySelector('.main__icon-copy');
+    const okIcon = this.querySelector('.main__icon-ok');
+    input.select();
+    document.execCommand('copy');
+    copyIcon.style.display = 'none';
+    okIcon.style.display = 'inline';
+    setTimeout(() => {
+      copyIcon.style.display = 'inline';
+      okIcon.style.display = 'none';
+    }, 500);
+  });
+});
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
